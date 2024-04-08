8861e759396b35134b2e1972d6be2ae900067b33
naporin0624 <naporin0624@gmail.com>
2024-04-08 12:47:32 +0000

    chore: use rpc sample

--- example/apps/workers/src/types.ts
+++ example/apps/workers/src/types.ts
@@ -1,5 +1,7 @@
+import type { YDurableObjects } from "y-durableobjects";
+
 export type Bindings = {
-  Y_DURABLE_OBJECTS: DurableObjectNamespace;
+  Y_DURABLE_OBJECTS: DurableObjectNamespace<YDurableObjects>;
 };
 
 declare module "hono" {
