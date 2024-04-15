8d9a0030637da15c4cfda0459260ec11f6f39536
naporin0624 <naporin0624@gmail.com>
2024-04-15 20:15:58 +0000

    chore: add vitest

---
+++ vite.config.ts
@@ -0,0 +1,15 @@
+import { defineWorkersProject } from "@cloudflare/vitest-pool-workers/config";
+
+export default defineWorkersProject({
+  test: {
+    globals: true,
+    poolOptions: {
+      workers: {
+        singleWorker: true,
+        wrangler: {
+          configPath: "./wrangler.test.toml",
+        },
+      },
+    },
+  },
+});
