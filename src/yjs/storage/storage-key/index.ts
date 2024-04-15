7c943092e6d27e03bb9a8d7f107450ad60e7290b
naporin0624 <naporin0624@gmail.com>
2024-04-15 20:16:14 +0000

    feat: create storage key encoder

---
+++ src/yjs/storage/storage-key/index.ts
@@ -0,0 +1,11 @@
+export type Key = {
+  type: "update";
+  name?: number;
+} | {
+  type: "state";
+  name: "bytes" | "doc" | "count";
+};
+
+export const storageKey = (key: Key) => {
+  return `ydoc:${key.type}:${key.name ?? ""}`;
+};
---
+++ src/yjs/storage/storage-key/storage-key.test.ts
@@ -0,0 +1,15 @@
+import { storageKey } from "."
+
+import type { Key } from "."
+
+describe("storageKey functionality", () => {
+  it.each([
+    [{ type: "update" }, "ydoc:update:"], // nameが省略された場合
+    [{ type: "update", name: 1 }, "ydoc:update:1"], // nameが数値で提供された場合
+    [{ type: "state", name: "bytes" }, "ydoc:state:bytes"], // typeがstateでnameがbytesの場合
+    [{ type: "state", name: "doc" }, "ydoc:state:doc"], // typeがstateでnameがdocの場合
+    [{ type: "state", name: "count" }, "ydoc:state:count"] // typeがstateでnameが新しく追加されたcountの場合
+  ])("correctly generates storage key for key: %o", (key, expected) => {
+    expect(storageKey(key as Key)).toEqual(expected);
+  });
+})
