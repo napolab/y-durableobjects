b949639d86215fac273f6a62eb70ec54e23c746f
naporin0624 <naporin0624@gmail.com>
2024-04-23 14:22:53 +0000

    chore: format storageKey

--- src/yjs/storage/storage-key/index.ts
+++ src/yjs/storage/storage-key/index.ts
@@ -1,10 +1,12 @@
-export type Key = {
-  type: "update";
-  name?: number;
-} | {
-  type: "state";
-  name: "bytes" | "doc" | "count";
-};
+export type Key =
+  | {
+      type: "update";
+      name?: number;
+    }
+  | {
+      type: "state";
+      name: "bytes" | "doc" | "count";
+    };
 
 export const storageKey = (key: Key) => {
   return `ydoc:${key.type}:${key.name ?? ""}`;
--- src/yjs/storage/storage-key/storage-key.test.ts
+++ src/yjs/storage/storage-key/storage-key.test.ts
@@ -1,6 +1,6 @@
-import { storageKey } from "."
+import { storageKey } from ".";
 
-import type { Key } from "."
+import type { Key } from ".";
 
 describe("storageKey functionality", () => {
   it.each([
@@ -8,8 +8,8 @@ describe("storageKey functionality", () => {
     [{ type: "update", name: 1 }, "ydoc:update:1"], // nameが数値で提供された場合
     [{ type: "state", name: "bytes" }, "ydoc:state:bytes"], // typeがstateでnameがbytesの場合
     [{ type: "state", name: "doc" }, "ydoc:state:doc"], // typeがstateでnameがdocの場合
-    [{ type: "state", name: "count" }, "ydoc:state:count"] // typeがstateでnameが新しく追加されたcountの場合
+    [{ type: "state", name: "count" }, "ydoc:state:count"], // typeがstateでnameが新しく追加されたcountの場合
   ])("correctly generates storage key for key: %o", (key, expected) => {
     expect(storageKey(key as Key)).toEqual(expected);
   });
-})
+});
